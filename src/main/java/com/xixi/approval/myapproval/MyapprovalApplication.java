4becdf95f41fce411a6f7e15209b422b8f5ea46f
shengchengchao <shengchengchao@ewell.cc>
2021-04-26 11:14:27 +0800

    feat(new) 初始化

---
+++ src/main/java/com/xixi/approval/myapproval/MyapprovalApplication.java
@@ -0,0 +1,13 @@
+package com.xixi.approval.myapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyapprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyapprovalApplication.class, args);
+    }
+
+}
